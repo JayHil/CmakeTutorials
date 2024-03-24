ba5f2d7673ca81e1a90914abdc2d26940e5d0b7f
JayHil <jaydenhilton8@gmail.com>
2024-03-24 12:47:40 -0700

    init commit

---
+++ Step1/build/CMakeFiles/tutorial.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/gheis/Desktop/Personal_projects/cmake-3.29.0-tutorial-source/Step1/tutorial.cxx" "CMakeFiles/tutorial.dir/tutorial.cxx.obj" "gcc" "CMakeFiles/tutorial.dir/tutorial.cxx.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
